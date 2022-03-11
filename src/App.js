aa09f57086269839db04edca69e845ec60db9905
Lumyrani <lumyrani@gmail.com>
2022-03-11 11:38:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Login from './Login';
+import Register from './Register';
+
+
+function App() {
+  return (
+    <main className="App">
+    <Register/>
+    <Login/>
+    </main>
+  );
+}
+
+export default App;
